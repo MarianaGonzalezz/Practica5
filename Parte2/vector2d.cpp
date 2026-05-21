d40a2d157558b61d09e84c26669421aa8dcd4b3c
Mariana <mariana.mgonzalez@udea.edu.co>
2026-05-21 00:28:21 -0500

    clase vector

--- Parte2/vector2d.cpp
+++ Parte2/vector2d.cpp
@@ -1,3 +1,40 @@
 #include "vector2d.h"
 
-vector2D::vector2D() {}
+vector2D::vector2D(double x, double y) : x(x), y(y) {}
+
+vector2D vector2D::operator+(const vector2D& v) const {
+    return vector2D(x + v.x, y + v.y);
+}
+
+vector2D vector2D::operator*(double escalar) const {
+    return vector2D(x * escalar, y * escalar);
+}
+
+vector2D vector2D::operator -(const vector2D& v) const{
+    return vector2D (
+        x - v.x,
+        y - v.y
+        );
+}
+
+double vector2D::modulo() const {
+    return std::sqrt(x * x + y * y);
+}
+
+double vector2D::productoPunto(const vector2D &v) const{
+    return x*v.x + y*v.y;
+}
+
+vector2D vector2D::normalizar() const{
+    double magnitud = modulo();
+
+    if (magnitud ==0){
+        return vector2D(0,0);
+    }
+
+    return vector2D (x/magnitud, y/magnitud);
+}
+
+double vector2D::distancia(const vector2D &v) const{
+    return (*this - v).modulo();
+}
--- Parte2/vector2d.h
+++ Parte2/vector2d.h
@@ -1,10 +1,32 @@
 #ifndef VECTOR2D_H
 #define VECTOR2D_H
 
-class vector2D
-{
+#include <cmath>
+
+
+class vector2D{
+private:
+    double x;
+    double y;
+
 public:
-    vector2D();
+
+    vector2D(double x=0, double y=0);
+
+    vector2D operator+(const vector2D& v ) const;
+    vector2D operator*(double escalar) const;
+    vector2D operator-(const vector2D& v) const;
+
+    double modulo() const;
+    double productoPunto(const vector2D& v) const;
+    vector2D normalizar() const;
+    double distancia (const vector2D& v) const;
+
+    double getX() const { return x; }
+    double getY() const { return y; }
+    void setX(double newX) { x = newX; }
+    void setY(double newY) { y = newY; }
+
 };
 
 #endif // VECTOR2D_H
